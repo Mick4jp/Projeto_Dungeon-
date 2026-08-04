7cd7cab1011d01111d5a4f5031fb4a3f489a4b01
Micka <mickaeljose95@gmail.com>
2026-08-04 00:19:51 -0300

    Inicio do projeto

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("Hello and welcome!");
+    System.out.println("Dungeon tyccon");
 
 }
