68c9876e792ca3c7c02efc710597bd4acf638255
Micka <mickaeljose95@gmail.com>
2026-08-16 23:04:02 -0300

    Adicionado menu com switch case, e adicionado informacao de moedas nos status.

--- src/Main.java
+++ src/Main.java
@@ -4,76 +4,79 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Moedas, Escolha, Sala, Construiu, Coluna, Linha;
-        String Nome;
-        int[][] Salas = new int[3][3];
+        int moedas, escolha, sala, construiu, coluna, linha;
+        String nome;
+        int[][] salas = new int[3][3];
 
-        Moedas = 100;
-        Sala = 30;
-        Construiu = 0;
+        moedas = 1000;
+        sala = 30;
+
+
+
         System.out.println("===================");
         System.out.println(" Dungeon Manager");
         System.out.println("===================");
         System.out.println();
         System.out.println("Digite o nome da dungeon: ");
-        Nome = sc.nextLine();
-        System.out.println("Bem-vindo a " + Nome + " ");
-        System.out.println("Voce possui " + Moedas + " moedas");
+        nome = sc.nextLine();
+        System.out.println("Bem-vindo a " + nome + " ");
+        System.out.println("Voce possui " + moedas + " moedas");
         System.out.println();
-        System.out.println("===================");
-        System.out.println(" Dungeon Manager");
-        System.out.println("===================");
-        System.out.println(" 1 - Comprar sala ");
-        System.out.println(" 2 - Ver status ");
-        System.out.println(" 3 - Sair ");
-        Escolha = sc.nextInt();
+
+        do {
+            construiu = 0;
+            System.out.println("===================");
+            System.out.println(" Dungeon Manager");
+            System.out.println("===================");
+            System.out.println(" 1 - Comprar sala ");
+            System.out.println(" 2 - Ver status ");
+            System.out.println(" 3 - Sair ");
+            escolha = sc.nextInt();
 
 
-        while (Escolha != 3) {
-            if (Escolha <= 3 && Escolha > 0) {
-                if (Escolha == 1) {
+            switch (escolha) {
 
-                    if (Moedas >= Sala) {
-                        for (Linha = 0; Linha < 3 && Construiu == 0; Linha++) {
-                            for (Coluna = 0; Coluna < 3 && Construiu == 0; Coluna++) {
-                                if (Salas[Linha][Coluna] == 0) {
-                                    Salas[Linha][Coluna] = 1;
+                case 1:
+
+                    if (moedas >= sala) {
+                        for (linha = 0; linha < 3 && construiu == 0; linha++) {
+                            for (coluna = 0; coluna < 3 && construiu == 0; coluna++) {
+                                if (salas[linha][coluna] == 0) {
+                                    salas[linha][coluna] = 1;
                                     System.out.println("Parabens!!! voce comprou uma sala");
-                                    Moedas -= Sala;
-                                    System.out.println(Moedas);
-                                    Construiu = 1;
+                                    moedas -= sala;
+                                    System.out.println(moedas);
+                                    construiu = 1;
 
                                 }
                             }
 
                         }
-                        if (Construiu == 0) {
+                        if (construiu == 0) {
                             System.out.println("Dungeon cheia!!!!!");
                         }
                     } else {
                         System.out.println("Moedas insuficiente!!!!!");
                     }
-                } else if (Escolha == 2) {
-                    for (Linha = 0; Linha < 3; Linha++) {
-                        for (Coluna = 0; Coluna < 3; Coluna++) {
-                            System.out.print(Salas[Linha][Coluna] + " ");
+                    break;
+                case 2:
+                    System.out.println("Moedas restantes: " + moedas);
+                    System.out.println("Voce comprou essas salas: ");
+                    for (linha = 0; linha < 3; linha++) {
+                        for (coluna = 0; coluna < 3; coluna++) {
+                            System.out.print(salas[linha][coluna] + " ");
                         }
                         System.out.println();
-                    }
+                    } break;
 
-                }
-            }else {
-                System.out.println("Escolha Invalida");
+                case 3: System.out.println("Obrigado Por ter Jogado, Volte sempre!!!!");
+                break;
 
+                default:
+                    System.out.println("Escolha invalida!!!");
             }
-                Construiu = 0;
-                System.out.println(" 1 - Comprar sala ");
-                System.out.println(" 2 - Ver status ");
-                System.out.println(" 3 - Sair ");
-                Escolha = sc.nextInt();
+        } while(escolha != 3 );
 
-        }
         sc.close();
-        System.out.println("Obrigado Por ter Jogado, Volte sempre!!!!");
     }
 }
